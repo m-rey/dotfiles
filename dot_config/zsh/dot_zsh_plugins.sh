18626cf36903561150887ff7fb11fb297bdc2137
Martin Rey <martin.rey@mailbox.org>
2020-06-10 01:36:21 +0200

    [antibody] Static loading to improve startup speed

---
+++ dot_config/zsh/dot_zsh_plugins.sh
@@ -0,0 +1,16 @@
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-changyuheng-SLASH-fz/fz.plugin.zsh
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-changyuheng-SLASH-fz )
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
+
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )

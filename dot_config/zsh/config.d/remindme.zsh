0e1dcbe6ed3a6446559c44c46acf20db8dc28a50
Martin Rey <martin.rey@mailbox.org>
2020-06-16 23:12:10 +0200

    [zsh] Ignore sighup for remindme messages

--- dot_config/zsh/config.d/remindme.zsh
+++ dot_config/zsh/config.d/remindme.zsh
@@ -1,3 +1,5 @@
 remindme() {
-	( sleep $2 && timeout ${3:-5} sm "$1" ) &
+  # usage: `remindme $message $timetowait $messageduration
+  # duration of message is 5s by default
+  nohup sh -c "sleep $2 && timeout ${3:-5} sm $1" > /dev/null 2>&1 &
 }
